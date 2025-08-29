320e506139fe2f9696e713a40f8022bfce4b4822
Álvaro Navarro <alvaro@thesnakebite.es>
2025-08-29 13:26:54 +0200

    the basics of validations

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -3,14 +3,30 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 
 class Greeter extends Component
 {
+    #[Validate('required|min:2|max:16')]
     public $name = '';
     public $greeting = '';
+    public $greetingMessage = '';
 
-    public function changeName()
-    {}
+    public function changeGreeting()
+    {
+        $this->validate();
+
+        $this->greetingMessage = "{$this->greeting}, {$this->name}";
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'El nombre es obligatorio',
+            'name.min' => 'El nombre debe contener mínimo 2 caracteres',
+            'name.max' => 'El nombre no puede exceder 16 caracteres',
+        ];
+    }
 
     public function render()
     {
